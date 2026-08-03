48ae12cf2cd142ad76cc48decab2a6eb393f8092
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-03 17:28:00 +0200

    RecruitmentAction implemented

---
+++ app/src/main/java/com/leaders/gamelogic/actions/RecruitmentAction.java
@@ -0,0 +1,33 @@
+package com.leaders.gamelogic.actions;
+
+import androidx.annotation.NonNull;
+
+import com.leaders.gamelogic.entities.Position;
+import com.leaders.gamelogic.enums.GameActionType;
+
+public final class RecruitmentAction implements IGameAction {
+    @Override
+    public GameActionType getActionType() {
+        return GameActionType.Recruitment;
+    }
+
+    @NonNull
+    private final Character character;
+    @NonNull
+    private final Position destPos;
+
+    public RecruitmentAction(@NonNull Character character, @NonNull Position destPos) {
+        this.character = character;
+        this.destPos = destPos;
+    }
+
+    @NonNull
+    public Character getCharacter() {
+        return character;
+    }
+
+    @NonNull
+    public Position getDestPos() {
+        return destPos;
+    }
+}
