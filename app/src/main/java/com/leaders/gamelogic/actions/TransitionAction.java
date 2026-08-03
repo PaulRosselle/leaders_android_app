574dba00a9fa85456726272267aa4d35a5bed334
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-03 16:54:41 +0200

    TransitionAction implemented

---
+++ app/src/main/java/com/leaders/gamelogic/actions/TransitionAction.java
@@ -0,0 +1,34 @@
+package com.leaders.gamelogic.actions;
+
+import androidx.annotation.NonNull;
+
+import com.leaders.gamelogic.enums.GameActionType;
+import com.leaders.gamelogic.enums.TransitionTarget;
+import com.leaders.gamelogic.enums.TransitionType;
+
+public class TransitionAction implements IGameAction {
+    @Override
+    public GameActionType getActionType() {
+        return GameActionType.Transition;
+    }
+
+    @NonNull
+    private final TransitionType transitionType;
+    @NonNull
+    private final TransitionTarget transitionTarget;
+
+    public TransitionAction(@NonNull TransitionType transitionType, @NonNull TransitionTarget transitionTarget) {
+        this.transitionType = transitionType;
+        this.transitionTarget = transitionTarget;
+    }
+
+    @NonNull
+    public TransitionType getTransitionType() {
+        return transitionType;
+    }
+
+    @NonNull
+    public TransitionTarget getTransitionTarget() {
+        return transitionTarget;
+    }
+}
