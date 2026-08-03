7fec00622681195bc49930c98ac6f478d22c0b04
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-03 17:19:17 +0200

    CharacterActionTarget implemented

---
+++ app/src/main/java/com/leaders/gamelogic/actions/CharacterActionTarget.java
@@ -0,0 +1,33 @@
+package com.leaders.gamelogic.actions;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.leaders.gamelogic.entities.Character;
+import com.leaders.gamelogic.entities.Position;
+
+public final class CharacterActionTarget {
+    @NonNull
+    private final Character character;
+    @Nullable
+    private final Position originPos;
+    @Nullable
+    private final Position destPos;
+    public CharacterActionTarget(@NonNull Character character, @Nullable Position originPos, @Nullable Position destPos) {
+        this.character = character;
+        this.originPos = originPos;
+        this.destPos = destPos;
+    }
+    @NonNull
+    public Character getCharacter() {
+        return character;
+    }
+    @Nullable
+    public Position getOriginPos() {
+        return originPos;
+    }
+    @Nullable
+    public Position getDestPos() {
+        return destPos;
+    }
+}
