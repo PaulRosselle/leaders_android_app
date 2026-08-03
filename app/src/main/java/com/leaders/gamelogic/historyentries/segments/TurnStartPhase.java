25e91eb653e77d9c5ddc44feb5c2d6edd193c4f5
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-03 18:04:57 +0200

    TurnStartPhase implemented

---
+++ app/src/main/java/com/leaders/gamelogic/historyentries/segments/TurnStartPhase.java
@@ -0,0 +1,34 @@
+package com.leaders.gamelogic.historyentries.segments;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.leaders.gamelogic.actions.IGameAction;
+import com.leaders.gamelogic.actions.TransitionAction;
+import com.leaders.gamelogic.enums.TransitionTarget;
+import com.leaders.gamelogic.historyentries.IPhase;
+import com.leaders.gamelogic.historyentries.Segment;
+
+import java.util.ArrayList;
+
+public final class TurnStartPhase extends Segment implements IPhase {
+    @NonNull
+    private final ArrayList<IGameAction> actions;
+
+    public TurnStartPhase(@Nullable TransitionAction startAction, @Nullable TransitionAction endAction) {
+        super(startAction, endAction);
+        actions = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public ArrayList<IGameAction> getActions() {
+        return actions;
+    }
+
+    @NonNull
+    @Override
+    public TransitionTarget getTransitionTarget() {
+        return TransitionTarget.TurnStartPhase;
+    }
+}
