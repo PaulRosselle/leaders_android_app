629753a30b15f2070ccbd5c3f83a269bbe4804d7
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-05 21:50:08 +0200

    TargetCategory implemented

---
+++ app/src/main/java/com/leaders/gamelogic/interactions/TargetCategory.java
@@ -0,0 +1,9 @@
+package com.leaders.gamelogic.interactions;
+
+public enum TargetCategory {
+    PlayableCharacter,
+    RecruitmentDestination,
+    MovementDestination,
+    ActiveAbilityDestination,
+    ActiveAbilityTarget
+}
