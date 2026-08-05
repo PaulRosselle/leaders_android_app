b26a1d1f50d59787716c1905cf555eb0630758d9
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-05 21:51:28 +0200

    InteractionResultType implemented

---
+++ app/src/main/java/com/leaders/gamelogic/interactions/InteractionResultType.java
@@ -0,0 +1,10 @@
+package com.leaders.gamelogic.interactions;
+
+public enum InteractionResultType {
+    CardChosen,
+    PositionChosen,
+    NoChoice,
+    CancelAction,
+    UndoLastAction,
+    EndPhase
+}
