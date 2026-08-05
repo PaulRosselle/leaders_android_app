f6b2252b3c2fc71fdba977bbad14888957216c09
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-05 14:09:52 +0200

    TransitionActionHandler implemented

---
+++ app/src/main/java/com/leaders/gamelogic/handlers/TransitionActionHandler.java
@@ -0,0 +1,28 @@
+package com.leaders.gamelogic.handlers;
+
+import androidx.annotation.NonNull;
+
+import com.leaders.gamelogic.actions.TransitionAction;
+import com.leaders.gamelogic.entities.Game;
+
+public final class TransitionActionHandler extends ActionHandler {
+    @NonNull
+    public TransitionAction transitionAction;
+
+    public TransitionActionHandler(@NonNull Game game, @NonNull TransitionAction transitionAction) {
+        super(game);
+        this.transitionAction = transitionAction;
+    }
+
+    @Override
+    public void doAction() {
+        // Since transitions have no impact on the game projection, we have no treatment to do here.
+        // This class is kept to simplify the generic approach on IGameAction
+    }
+
+    @Override
+    public void undoAction() {
+        // Since transitions have no impact on the game projection, we have no treatment to do here.
+        // This class is kept to simplify the generic approach on IGameAction
+    }
+}
