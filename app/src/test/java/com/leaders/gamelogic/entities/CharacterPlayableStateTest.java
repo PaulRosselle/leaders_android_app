5c1c6c76db8e7b6a1018a81c7fe06d75f3af5ac2
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-07 11:37:03 +0200

    CharacterPlayableStateTest implemented

---
+++ app/src/test/java/com/leaders/gamelogic/entities/CharacterPlayableStateTest.java
@@ -0,0 +1,84 @@
+package com.leaders.gamelogic.entities;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import com.leaders.gamelogic.enums.CharacterType;
+import com.leaders.gamelogic.enums.TeamColor;
+
+import org.junit.Test;
+
+public class CharacterPlayableStateTest {
+
+    @Test
+    public void constructor_shouldStoreCharacter() {
+        Character character = Character.create(
+                CharacterType.LeaderQueen,
+                TeamColor.Black
+        );
+        Position position = new Position(3, 3);
+
+        CharacterPlayableState state =
+                new CharacterPlayableState(character, position, true, false);
+
+        assertSame(character, state.getCharacter());
+    }
+
+    @Test
+    public void constructor_shouldStorePosition() {
+        Character character = Character.create(
+                CharacterType.LeaderQueen,
+                TeamColor.Black
+        );
+        Position position = new Position(3, 3);
+
+        CharacterPlayableState state =
+                new CharacterPlayableState(character, position, true, false);
+
+        assertSame(position, state.getPosition());
+    }
+
+    @Test
+    public void constructor_shouldStoreMandatoryState() {
+        Character character = Character.create(
+                CharacterType.LeaderQueen,
+                TeamColor.Black
+        );
+        Position position = new Position(3, 3);
+
+        CharacterPlayableState state =
+                new CharacterPlayableState(character, position, true, false);
+
+        assertTrue(state.isMandatory());
+    }
+
+    @Test
+    public void constructor_shouldStoreCanUseActiveAbilityState() {
+        Character character = Character.create(
+                CharacterType.LeaderQueen,
+                TeamColor.Black
+        );
+        Position position = new Position(3, 3);
+
+        CharacterPlayableState state =
+                new CharacterPlayableState(character, position, false, true);
+
+        assertTrue(state.isCanUseActiveAbility());
+    }
+
+    @Test
+    public void constructor_shouldStoreFalseBooleanStates() {
+        Character character = Character.create(
+                CharacterType.LeaderQueen,
+                TeamColor.Black
+        );
+        Position position = new Position(3, 3);
+
+        CharacterPlayableState state =
+                new CharacterPlayableState(character, position, false, false);
+
+        assertFalse(state.isMandatory());
+        assertFalse(state.isCanUseActiveAbility());
+    }
+}
