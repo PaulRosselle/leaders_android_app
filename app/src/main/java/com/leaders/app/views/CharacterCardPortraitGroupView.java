a58533e6cad1cd92f31fdbca882e078a7e377c2b
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-18 22:15:00 +0200

    CharacterCardPortraitView and CharacterCardPortraitGroupView implemented

---
+++ app/src/main/java/com/leaders/app/views/CharacterCardPortraitGroupView.java
@@ -0,0 +1,111 @@
+package com.leaders.app.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.leaders.R;
+import com.leaders.gamelogic.enums.CharacterCard;
+
+import java.util.ArrayList;
+
+public class CharacterCardPortraitGroupView extends LinearLayout {
+    private static final int PORTRAIT_DEFAULT_MARGIN = 2;
+
+    private final ArrayList<CharacterCardPortraitView> characterCardPortraitViews;
+    private OnClickListener onPortraitClickListener;
+    private OnLongClickListener onPortraitLongClickListener;
+    private int portraitMargins;
+
+
+    public CharacterCardPortraitGroupView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        characterCardPortraitViews = new ArrayList<>();
+        onPortraitClickListener = null;
+        onPortraitLongClickListener = null;
+        portraitMargins = PORTRAIT_DEFAULT_MARGIN;
+
+        try (TypedArray customAttrs = context.obtainStyledAttributes(attrs, R.styleable.CharacterCardPortraitGroupView)) {
+            int groupSize = customAttrs.getInteger(R.styleable.CharacterCardPortraitGroupView_maxGroupSize, 6);
+            setPortraits(new ArrayList<>(), groupSize);
+        }
+    }
+
+    public CharacterCardPortraitGroupView(@NonNull Context context,
+                                          @NonNull ArrayList<CharacterCard> portraitCards,
+                                          int groupSize) {
+        this(context, null);
+        setPortraits(portraitCards, groupSize);
+    }
+
+    public void setPortraits(@NonNull ArrayList<CharacterCard> portraitCards, int groupSize) {
+        // The smallest value for group max size is 2 since it wouldn't make
+        // sense to create a group view for less than two portraits
+        groupSize = Math.max(groupSize, 2);
+
+        characterCardPortraitViews.clear();
+        removeAllViews();
+        for (int i = 0; i < groupSize; i++) {
+            CharacterCardPortraitView portraitView = new CharacterCardPortraitView(getContext(), null);
+
+            if (i < portraitCards.size()) {
+                portraitView.setVisibility(VISIBLE);
+                portraitView.setPortraitCard(portraitCards.get(i));
+            } else {
+                portraitView.setVisibility(INVISIBLE);
+            }
+
+            addView(portraitView, getPortraitLayoutParams());
+            characterCardPortraitViews.add(portraitView);
+        }
+
+        updatePortraitsClickListener();
+        updatePortraitsLongClickListener();
+    }
+
+    public void setPortraitMargins(int portraitMargins) {
+        this.portraitMargins = portraitMargins;
+        for (CharacterCardPortraitView ptvPortrait : characterCardPortraitViews) {
+            ptvPortrait.setLayoutParams(getPortraitLayoutParams());
+        }
+    }
+
+    private LinearLayout.LayoutParams getPortraitLayoutParams() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        int marginValueInPx = (int) (portraitMargins * getResources().getDisplayMetrics().density);
+        layoutParams.setMarginStart(marginValueInPx);
+        layoutParams.setMarginEnd(marginValueInPx);
+        layoutParams.weight = 1;
+        return layoutParams;
+    }
+
+    public void setPortraitsClickListener(OnClickListener onPortraitClickListener) {
+        this.onPortraitClickListener = onPortraitClickListener;
+        updatePortraitsClickListener();
+    }
+
+    public void setPortraitsLongClickListener(OnLongClickListener onPortraitLongClickListener) {
+        this.onPortraitLongClickListener = onPortraitLongClickListener;
+    }
+
+    private void updatePortraitsClickListener() {
+        for (CharacterCardPortraitView characterCardPortraitView : characterCardPortraitViews) {
+            characterCardPortraitView.setOnClickListener(onPortraitClickListener);
+        }
+    }
+
+    private void updatePortraitsLongClickListener() {
+        for (CharacterCardPortraitView characterCardPortraitView : characterCardPortraitViews) {
+            characterCardPortraitView.setOnLongClickListener(onPortraitLongClickListener);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/leaders/app/views/CharacterCardPortraitView.java
@@ -0,0 +1,53 @@
+package com.leaders.app.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.leaders.R;
+import com.leaders.gamelogic.enums.CharacterCard;
+
+public class CharacterCardPortraitView extends AppCompatImageView {
+    @NonNull
+    private CharacterCard portraitCard;
+
+    // PortraitType incoming in a future ticket (Default, Banned, Hexagonal) but not currently needed
+
+    public CharacterCardPortraitView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        // Nemesis is the default portrait (because I really like her design)
+        setPortraitCard(CharacterCard.Nemesis);
+    }
+
+    public void setPortraitCard(@NonNull CharacterCard portraitCard) {
+        this.portraitCard = portraitCard;
+        setImageResource(getPortraitDrawableId());
+    }
+
+    private int getPortraitDrawableId() {
+        switch (portraitCard) {
+            case Acrobat: return R.drawable.card_portrait_acrobat;
+            case Archer: return R.drawable.card_portrait_archer;
+            case Assassin: return R.drawable.card_portrait_assassin;
+            case Brewmaster: return R.drawable.card_portrait_brewmaster;
+            case Bruiser: return R.drawable.card_portrait_bruiser;
+            case ClawLauncher: return R.drawable.card_portrait_claw_launcher;
+            case HermitAndCub: return R.drawable.card_portrait_hermit_and_cub;
+            case Illusionist: return R.drawable.card_portrait_illusionist;
+            case Jailer: return R.drawable.card_portrait_jailer;
+            case LeaderKing: return R.drawable.card_portrait_leader_king;
+            case LeaderQueen: return R.drawable.card_portrait_leader_queen;
+            case Manipulator: return R.drawable.card_portrait_manipulator;
+            case Nemesis: return R.drawable.card_portrait_nemesis;
+            case Protector: return R.drawable.card_portrait_protector;
+            case Rider: return R.drawable.card_portrait_rider;
+            case RoyalGuard: return R.drawable.card_portrait_royal_guard;
+            case Vizier: return R.drawable.card_portrait_vizier;
+            case Wanderer: return R.drawable.card_portrait_wanderer;
+            default: throw new IllegalArgumentException("No portrait drawable for card: " + portraitCard);
+        }
+    }
+}
