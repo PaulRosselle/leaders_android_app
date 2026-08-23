3b1a5fec914b0d7c9034d90a9381625f68bc6721
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-23 13:49:45 +0200

    ReadOnlyBoardView implemented

---
+++ app/src/main/java/com/leaders/app/views/board/ReadOnlyBoardView.java
@@ -0,0 +1,29 @@
+package com.leaders.app.views.board;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.leaders.R;
+
+public final class ReadOnlyBoardView extends BoardView {
+    public ReadOnlyBoardView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setOnCharacterLongClickListener(OnLongClickListener onCharacterLongClickListener) {
+        // LongClickListener setter is visible to allow for character description display
+        super.setOnCharacterLongClickListener(onCharacterLongClickListener);
+    }
+
+    public void setCellPositionVisible(boolean visible) {
+        if (visible) {
+            imvBoard.setForeground(ContextCompat.getDrawable(getContext(), R.drawable.cell_positions));
+        } else {
+            imvBoard.setForeground(null);
+        }
+    }
+}
