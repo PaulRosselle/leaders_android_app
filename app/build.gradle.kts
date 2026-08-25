b0b21422450f890f73d90725ed62ca3ca98a9011
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-25 16:10:17 +0200

    version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.paulrosselle.leaders"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "v0.3.0"
+        versionCode = 2
+        versionName = "v0.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
