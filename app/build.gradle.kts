8d08326727523d7939e2f85769e830debdcfa817
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-26 11:06:36 +0200

    Gradle and SDK updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.leaders"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.paulrosselle.leaders"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 2
         versionName = "v0.4.0"
 
@@ -26,15 +26,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
 dependencies {
 
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.activity:activity-ktx:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.8.0")
     implementation("androidx.documentfile:documentfile:1.1.0")
     implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.2")
