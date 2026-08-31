bb68832a8e0bbdd438aa94622611c836b007cd0c
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-08-31 20:28:01 +0200

    DuelPlayerActivity view listeners added

--- app/src/main/java/com/leaders/app/activities/duel/DuelPlayerActivity.java
+++ app/src/main/java/com/leaders/app/activities/duel/DuelPlayerActivity.java
@@ -13,15 +13,27 @@ import com.leaders.R;
 import com.leaders.app.activities.BaseActivity;
 import com.leaders.app.enums.ActivityType;
 import com.leaders.app.views.board.PlayableBoardView;
+import com.leaders.app.views.character.CharacterCardPortraitView;
+import com.leaders.app.views.character.CharacterNotificationView;
+import com.leaders.app.views.character.CharacterView;
 import com.leaders.app.views.duel.CharacterCardSelectionView;
 import com.leaders.app.views.duel.PlayerBottomView;
 import com.leaders.app.views.duel.PlayerTopView;
+import com.leaders.gamelogic.enums.CharacterCard;
+import com.leaders.gamelogic.enums.CharacterType;
+import com.leaders.gamelogic.interactions.InteractionTarget;
 
-public class DuelPlayerActivity extends BaseActivity {
+import java.util.Objects;
+
+public class DuelPlayerActivity extends BaseActivity implements
+        PlayableBoardView.OnTargetClickListener,
+        CharacterCardSelectionView.OnCardSelectedListener {
     private PlayableBoardView bdvBoard;
     private CharacterCardSelectionView ccsvCardSelector;
     private PlayerBottomView pbvCurrentPlayer;
     private PlayerTopView ptvOpposingPlayer;
+
+    private CharacterNotificationView cnvCardInfo;
 
     private MaterialButton btnActions;
     private MaterialButton btnCards;
@@ -43,6 +55,8 @@ public class DuelPlayerActivity extends BaseActivity {
         pbvCurrentPlayer = findViewById(R.id.pbvCurrentPlayer_actDuelPlayer);
         ptvOpposingPlayer = findViewById(R.id.ptvOpposingPlayer_actDuelPlayer);
 
+        cnvCardInfo = findViewById(R.id.cnvCardInfo_actPuzzlePlayer);
+
         btnActions = findViewById(R.id.btnActions_actDuelPlayer);
         btnCards = findViewById(R.id.btnCards_actDuelPlayer);
         btnUndoLastAction = findViewById(R.id.btnUndoLastAction_actDuelPlayer);
@@ -54,8 +68,16 @@ public class DuelPlayerActivity extends BaseActivity {
     protected void initListeners() {
         super.initListeners();
 
+        bdvBoard.setOnTargetClickListener(this);
+        bdvBoard.setOnCharacterLongClickListener(this::onBoardCharacterLongClick);
+
+        ccsvCardSelector.setOnCardSelectedListener(this);
+        ccsvCardSelector.setOnPortraitLongClickListener(this::onPortraitLongClick);
+
+        btnActions.setOnClickListener(this::onActionsClick);
         btnCards.setOnClickListener(this::onCardsClick);
-        // TODO
+        btnUndoLastAction.setOnClickListener(this::onUndoLastActionClick);
+        btnNextPhase.setOnClickListener(this::onNextPhaseClick);
     }
 
     @Override
@@ -111,18 +133,67 @@ public class DuelPlayerActivity extends BaseActivity {
     }
 
     private void onUndoLastActionClick(View v) {
-        // TODO
+        // TODO - undo last action
     }
 
     private void onNextPhaseClick(View v) {
-        // TODO
+        // TODO - next phase
+    }
+
+    private void onActionsClick(View v) {
+        // TODO - show actions menu
+    }
+
+    private boolean onBoardCharacterLongClick(View v) {
+        CharacterType characterType = Objects.requireNonNull(((CharacterView) v).getCharacterType(),
+                "An empty character piece is not authorized in the puzzle editor");
+        showCardDescriptionNotification(characterType.getCharacterCard());
+        return true;
+    }
+
+    private boolean onPortraitLongClick(View v) {
+        showCardDescriptionNotification(((CharacterCardPortraitView) v).getPortraitCard());
+        return true;
     }
 
     //endregion
 
+    //region INTERACTION METHODS
+
+    @Override
+    public void onEmptyClick() {
+
+    }
+
+    @Override
+    public void onTargetClick(@NonNull InteractionTarget target) {
+        // TODO - handle actions phase
+    }
+
+    public void onRecruitmentCardSelected(@NonNull InteractionTarget target) {
+        // TODO - handle recruitment phase
+    }
+    public void onBanishmentCardSelected(@NonNull InteractionTarget target) {
+        // TODO - handle banishment phase
+    }
+
+    //endregion
+
+    private void showCardDescriptionNotification(@NonNull CharacterCard characterCard) {
+        if (cnvCardInfo.getCharacterCard() == characterCard) {
+            cnvCardInfo.setCharacterCard(null);
+            cnvCardInfo.hide();
+        } else {
+            cnvCardInfo.setCharacterCard(characterCard);
+            if (cnvCardInfo.getVisibility() != View.VISIBLE) {
+                cnvCardInfo.show();
+            }
+        }
+    }
+
     private void setCardSelectorVisible(boolean visible) {
-        // When recruiting, we display the recruitmentView below currentPlayerView.
-        // The layout transition is animated for both the board and currentPlayerView
+        // When recruiting, we display the cardSelector view below the current player view.
+        // The layout transition is animated for both the board and current player view
         bdvBoard.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
         pbvCurrentPlayer.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
 
@@ -143,7 +214,7 @@ public class DuelPlayerActivity extends BaseActivity {
                     playerHeaderHeight * 2 + playerViewMargin * dpRatio);
             ccsvCardSelector.show(true);
 
-        // By default, each playerView is on a vertical extremity while the board is centered
+            // By default, each playerView is on a vertical extremity while the board is centered
         } else {
             boardParams.verticalBias = 0.5f;
             playerViewParams.verticalBias = 1f;
