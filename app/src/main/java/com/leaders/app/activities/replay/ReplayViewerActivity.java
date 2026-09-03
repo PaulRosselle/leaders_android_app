6aae3bd45a47f4c11f74b4a8c9a3e628844b4814
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-09-03 22:34:50 +0200

    ReplayViewerActivity now inherits BaseActivity

--- app/src/main/java/com/leaders/app/activities/replay/ReplayViewerActivity.java
+++ app/src/main/java/com/leaders/app/activities/replay/ReplayViewerActivity.java
@@ -1,6 +1,68 @@
 package com.leaders.app.activities.replay;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-public class ReplayViewerActivity extends AppCompatActivity {
+import com.leaders.R;
+import com.leaders.app.activities.BaseActivity;
+import com.leaders.app.enums.ActivityType;
+
+public class ReplayViewerActivity extends BaseActivity {
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        // TODO
+    }
+
+
+    @Override
+    protected void initListeners() {
+        super.initListeners();
+        // TODO
+    }
+
+
+    @Override
+    protected void initDatas() {
+        super.initDatas();
+        // TODO
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_replay_viewer;
+    }
+
+    @Override
+    protected int getRootGuidelineResId() {
+        return R.id.gdlRoot_actReplayViewer;
+    }
+
+    @Nullable
+    @Override
+    protected Integer getBtnBackResId() {
+        return R.id.btnBack_actReplayViewer;
+    }
+
+    @Override
+    protected boolean isImmersiveActivity() {
+        return true;
+    }
+
+    @Override
+    protected boolean overrideOnBackPressed() {
+        return true;
+    }
+
+    @Override
+    protected boolean askForConfirmationBeforeFinish() {
+        return false;
+    }
+
+    @NonNull
+    @Override
+    public ActivityType getActivityType() {
+        return ActivityType.ReplayViewer;
+    }
 }
--- app/src/main/java/com/leaders/app/enums/ActivityType.java
+++ app/src/main/java/com/leaders/app/enums/ActivityType.java
@@ -12,6 +12,7 @@ import com.leaders.app.activities.puzzle.PuzzleEditorActivity;
 import com.leaders.app.activities.puzzle.PuzzlePlayerActivity;
 import com.leaders.app.activities.puzzle.PuzzleSelectionActivity;
 import com.leaders.app.activities.puzzle.PuzzleSolverActivity;
+import com.leaders.app.activities.replay.ReplayViewerActivity;
 
 import java.util.NoSuchElementException;
 
@@ -24,7 +25,9 @@ public enum ActivityType {
     PuzzlePlayer,
     // DUEL
     DuelSetup,
-    DuelPlayer;
+    DuelPlayer,
+    // REPLAY
+    ReplayViewer;
 
     @NonNull
     public Intent getIntent(@NonNull Context context) {
@@ -36,6 +39,7 @@ public enum ActivityType {
             case PuzzlePlayer: return new Intent(context, PuzzlePlayerActivity.class);
             case DuelSetup: return new Intent(context, DuelSetupActivity.class);
             case DuelPlayer: return new Intent(context, DuelPlayerActivity.class);
+            case ReplayViewer: return new Intent(context, ReplayViewerActivity.class);
             default: throw new NoSuchElementException(String.format("No class found matching %s", this));
         }
     }
