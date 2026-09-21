4a50252e6cd87c96c624023763ab78d3dfb397f9
PaulRosselle <paul.rosselle5962@gmail.com>
2026-09-21 13:05:52 +0200

    Passive abilities tutoriel updated

--- app/src/main/java/com/leaders/app/activities/rules/tutorial/RulesTutorialPassiveAbilitiesActivity.java
+++ app/src/main/java/com/leaders/app/activities/rules/tutorial/RulesTutorialPassiveAbilitiesActivity.java
@@ -1,9 +1,16 @@
 package com.leaders.app.activities.rules.tutorial;
 
+import android.text.Spannable;
+import android.text.style.ImageSpan;
+
 import androidx.annotation.NonNull;
 
+import com.leaders.R;
 import com.leaders.app.enums.ActivityType;
 import com.leaders.app.enums.TutorialChapter;
+import com.leaders.app.utilities.SpannableUtils;
+
+import java.util.List;
 
 public class RulesTutorialPassiveAbilitiesActivity extends RulesTutorialActivity {
     @NonNull
@@ -15,5 +22,36 @@ public class RulesTutorialPassiveAbilitiesActivity extends RulesTutorialActivity
     @Override
     public TutorialChapter getChapter() {
         return TutorialChapter.PassiveAbilities;
+    }
+
+    protected Spannable getTextBeforeSpannable() {
+        int iconSize = getResources().getDimensionPixelSize(R.dimen.image_span_default_size);
+
+        ImageSpan iconSpan = SpannableUtils.getImageSpan(
+                this,
+                R.drawable.icon_ability_passive,
+                iconSize,
+                iconSize
+        );
+
+        ImageSpan acrobatSpan = SpannableUtils.getImageSpan(
+                this,
+                R.drawable.character_piece_vizier_b,
+                iconSize,
+                iconSize
+        );
+
+        ImageSpan illusionistSpan = SpannableUtils.getImageSpan(
+                this,
+                R.drawable.character_piece_leader_king_b,
+                iconSize,
+                iconSize
+        );
+
+        return SpannableUtils.getImageSpannable(
+                this,
+                getChapter().getTextBeforeResId(),
+                List.of(iconSpan, acrobatSpan, illusionistSpan)
+        );
     }
 }
